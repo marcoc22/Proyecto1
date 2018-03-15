8e284b3669ff9b57edb1ac482f94796a6b057fc6
Ronald9015 <36794407+Ronald9015@users.noreply.github.com>
2018-03-15 00:14:52 -0600

    Todo lo que había hecho
    El que lea esto es puto

---
+++ home.php
@@ -0,0 +1,23 @@
+<?php 
+
+?>
+
+<html>
+      <head> 
+	    <link href="estilos/login.css" rel="stylesheet" type="text/css">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Pantalla principal de servicio de Gestión </title> 
+      </head>
+<body>
+
+
+<div>
+
+<p> Hola doggo, esta es una página de prueba </p>
+
+</div>
+
+</body>
+
+</html>
