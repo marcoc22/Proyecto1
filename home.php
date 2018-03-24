b323e5a50354819ddd408331f5c9c4010831e1b1
marco <marco@DESKTOP-O9DPP57>
2018-03-23 23:57:27 -0600

    Falta subir mp3 y editar entrada.

--- home.php
+++ home.php
@@ -4,6 +4,32 @@ session_start();
 $contenidoPath = "database/contenido.txt";
 $indicePath = "database/indice.txt";
 
+
+//***********************************ELIMINANDO ENTRADA***************************************
+if (isset($_POST['eliminar'])) {
+    $actual = (int) $_POST['id'];
+    $detalleIndexUpdate = fopen($indicePath, "r+");
+    $pos = 0;
+    while (!feof($detalleIndexUpdate)) {
+        $line = fgets($detalleIndexUpdate);
+        $pos += strlen($line);
+        if (!empty($line)) {
+            $datos = explode(",", $line);
+            if ($datos[0] == $actual) {
+                $datos[1] = 0;
+                $nuevaLinea = implode(",", $datos) . "\n";
+                fseek($detalleIndexUpdate, $pos - strlen($line), SEEK_SET);
+                fwrite($detalleIndexUpdate, $nuevaLinea);
+                $arrayIndex[$datos[0]] = $datos;
+                break;
+            }
+        }
+        unset($line);
+    }
+    fclose($detalleIndexUpdate);
+}
+
+//**************************************LEYENDO ENTRADAS**************************************
 $detalleArchivo = fopen($contenidoPath, "r");
 $detalleIndex = fopen($indicePath, "r");
 $arrayIndex = array();
@@ -24,14 +50,9 @@ while (!feof($detalleIndex)) {
     }
     unset($line);
 }
+fclose($detalleArchivo);
+fclose($detalleIndex);
 unset($file);
-
-
-
-
-
-
-
 
 
 
@@ -58,12 +79,13 @@ include('comun/header.php');
             <tbody>
                 <?php
                 while (count($arrayContenido) > 0) {
-                    echo "<form action='' method='post' enctype='multipart/form-data'>";
+
                     $btnEliminar = "<input type='submit' name='eliminar' value='Eliminar'>";
                     $btnEditar = "<input type='submit' name='editar' value='Editar'>";
-                    $btnMp3 = '<input type="file" id="archivo" name="archivo"  accept=".mp3" style="width:134px;" required>';
+                    $btnMp3 = '<input type="file" id="archivo" name="archivo"  accept=".mp3" style="width:134px;">';
                     $row = array_pop($arrayContenido);
                     echo '<tr>';
+                    
                     echo "<td>{$row[1]}</td>";
                     echo "<td>{$row[2]}</td>";
                     echo "<td>{$row[3]}</td>";
@@ -71,14 +93,14 @@ include('comun/header.php');
                     echo "<td>{$row[5]}</td>";
                     echo "<td>{$row[6]}</td>";
                     echo '<td>';
-                    echo strlen($row[7])<=2?$btnMp3:"<span>{$row[7]}</span>";
+                    echo "<form action='' method='post' enctype='multipart/form-data'>";
+                    echo strlen($row[7]) <= 2 ? $btnMp3 : "<span>{$row[7]}</span>";
                     echo $btnEditar;
                     echo $btnEliminar;
+                    echo '<input type="hidden" name="id" value="' . $row[0] . '" >';
+                    echo "</form>";
                     echo '</td>';
                     echo '</tr>';
-                    echo '<input type="hidden" name="id" value="'.$row[0].'"';
-                    echo '</form>';
-                    
                 }
                 ?>
             </tbody>
