523da5981844136df2c6decf0fd8bc658685cecf
Ronald9015 <36794407+Ronald9015@users.noreply.github.com>
2018-04-03 13:47:58 -0600

    Se leen entradas y se esconden los elementos
    Muy explícito el título

--- home.php
+++ home.php
@@ -3,7 +3,53 @@ session_start();
 
 $contenidoPath = "database/contenido.txt";
 $indicePath = "database/indice.txt";
+$modoEdicion = false;
 
+/*
+*/
+
+$arrayActual = array();
+
+//************************************EDITANDO ENTRADA****************************************
+
+if (isset($_POST['editar'])) {
+    
+    $divStyle="display:block;";
+    $divStyle2="display:none;";
+    
+    
+    $actual = (int) $_POST['id'];
+    $detalleIndexUpdate = fopen($indicePath, "r+");
+    $detalleArchivoUpdate = fopen($contenidoPath, "r+");
+
+      
+    while (!feof($detalleIndexUpdate)) {
+
+        $line = fgets($detalleIndexUpdate);
+        if (!empty($line)) {
+            $lines = explode(";", $line);
+            foreach ($lines as $curLine) {
+                if (!empty($curLine)) {
+                    $datos = explode(",", $curLine);
+                    if ($datos[0] == $actual) {
+                        $lineaDato = (int) $datos[1]; //donde inicia
+                        fseek($detalleArchivoUpdate, $lineaDato);
+                        $fila = fread($detalleArchivoUpdate, intval($datos[2]));
+                        array_push($arrayActual, explode(",", $fila));
+                    }
+                }
+            }
+        }
+    }
+     
+
+}
+
+ else {
+     $divStyle="display:none;";
+     $divStyle2="display:block;";
+ }
+     
 
 //***********************************ELIMINANDO ENTRADA***************************************
 if (isset($_POST['eliminar'])) {
@@ -70,7 +116,30 @@ fclose($detalleIndex);
 include('comun/header.php');
 ?>
 
-<div class="container-list">
+<div class="container" style="<?php echo $divStyle ?>">
+    <a href="home.php">Volver al listado</a>
+    <div class="form-style-6" align="center">
+        <h1>Datos de mp3</h1>
+        <font color="<?php echo isset($alerta) ? 'red' : 'green'; ?>"><?php echo isset($alerta) ? $alerta : $success; ?></font>
+         <?php
+                while (count($arrayActual) > 0) {
+                    $filaActual = array_pop($arrayActual);       
+                }
+                
+         ?>
+        <form action='' method='post' enctype='multipart/form-data'>
+            <input type="text" id="nombre" value="<?php echo $filaActual[1]; ?>" name="nombre" placeholder="Nombre" required>
+            <!--<input type="file" id="archivo" name="archivo"  accept=".mp3" required>-->
+            <input type="text" id="autor" name="autor" value ="<?php echo $filaActual[2]; ?>"placeholder="Autor" required>
+            <input type="date" id="fecha" name="fecha" value="<?php echo $filaActual[3]; ?>" placeholder="Fecha" required >
+            <input type="text" id="clasificacion" value="<?php echo $filaActual[5]; ?>" name="clasificacion" placeholder='Clasificación' required>
+            <input type="text" name="descripcion" value="<?php echo $filaActual[6]; ?>" placeholder="Descripción" required>
+            <input type="submit" name="guardar" value="Guardar">
+        </form>
+    </div>
+</div>
+
+<div class="container-list" style="<?php echo $divStyle2 ?>">
     <a href="nueva_entrada.php">Agregar Nuevo</a>
     <div class="form-style-8" align="center">
         <h1>Mis archivos mp3</h1>
