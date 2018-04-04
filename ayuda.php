5f783eb605df19228fdb61fdcb22d98e5bd1731d
marco <marco@DESKTOP-O9DPP57>
2018-04-04 07:43:02 -0600

    Guardar en home

---
+++ ayuda.php
@@ -0,0 +1,13 @@
+<?php
+include('comun/header.php');
+?>
+
+<div class="container-list" style="<?php echo $divStyle2 ?>">
+    <a href="nueva_entrada.php">Agregar Nuevo</a>
+    <div class="form-style-8" align="center">
+        <h1>Mis archivos mp3</h1>
+        
+    </div>
+</div>
+<?php
+include('comun/footer.php');
--- home.php
+++ home.php
@@ -1,28 +1,137 @@
 <?php
 session_start();
-
+$success = '';
+if (!isset($_SESSION['usuario'])) {
+    session_destroy();
+    header('Location: index.php');
+    exit();
+}
 $contenidoPath = "database/contenido.txt";
 $indicePath = "database/indice.txt";
 $modoEdicion = false;
+$modoGuardar = FALSE;
 
 /*
-*/
+ */
 
 $arrayActual = array();
 
-//************************************EDITANDO ENTRADA****************************************
+//************************************GUARDANDO ENTRADA****************************************
+if(isset($_POST['nuevo'])){
+   $divStyle = "display:none;";
+    $divStyle2 = "display:none;";
+    $divStyleGuardar = "display:block;"; 
+}
+else if (isset($_POST['guardar'])) {//vamos a guardar los datos del formulario en el archivo y la carpeta que corresponde al usuario logueado
+    $modoGuardar = TRUE;
+    $divStyle = "display:none;";
+    $divStyle2 = "display:none;";
+    $divStyleGuardar = "display:block;"; 
+    $detalleIndex = fopen($indicePath, "r+");
+    $arrayIndex = array();
+    $arrayDisponibles = array(); //almaceno los campos de archivo que fueron borrados pero puedo utilizarlos para almacenar
+    $ultimaLinea = 0;
+    $arrayIndicesUsados = array();
+    while (!feof($detalleIndex)) {
+        $line = fgets($detalleIndex);
 
-if (isset($_POST['editar'])) {
-    
-    $divStyle="display:block;";
-    $divStyle2="display:none;";
-    
-    
+        if (!empty($line)) {
+            $lines = explode(";", $line);
+            foreach ($lines as $curLine) {
+                if (!empty($curLine)) {
+                    $datos = explode(",", $curLine);
+                    if ($datos[3] == 1) {
+                        array_push($arrayIndicesUsados, $datos[0]);
+                    }
+                }
+            }unset($curLine);
+        }
+        unset($line);
+    }
+    fclose($detalleIndex);
+    $detalleIndex = fopen($indicePath, "r+");
+    while (!feof($detalleIndex)) {
+        $line = fgets($detalleIndex);
+
+        if (!empty($line)) {
+            $lines = explode(";", $line);
+            foreach ($lines as $curLine) {
+                if (!empty($curLine)) {
+                    $datos = explode(",", $curLine);
+                    if ($datos[3] == 1) {
+                        $arrayIndex[$datos[0]] = $datos;
+                    } else {
+                        if (!in_array($datos[0], $arrayIndicesUsados)) {
+                            array_push($arrayDisponibles, $datos);
+                        }
+                    }
+                    $ultimaLinea++;
+                }
+            }unset($curLine);
+        }
+        unset($line);
+    }
+
+
+
+    $nombre = isset($_POST['nombre']) ? $_POST['nombre'] : '';
+    $autor = isset($_POST['autor']) ? $_POST['autor'] : '';
+    $fecha = isset($_POST['fecha']) ? $_POST['fecha'] : '';
+    $clasificacion = isset($_POST['clasificacion']) ? $_POST['clasificacion'] : '';
+    $descripcion = isset($_POST['descripcion']) ? $_POST['descripcion'] : '';
+
+
+    if (empty($nombre)) {
+        $alerta .= 'El nombre no puede estar vacío<br>';
+    } else {
+        if (!isset($alerta)) {
+            $datoLinea = "{$ultimaLinea},{$nombre},{$autor},{$fecha},,{$clasificacion},{$descripcion},";
+            $tamContenido = strlen($datoLinea);
+
+            $detalleArchivo = fopen($contenidoPath, "r+"); //abro en modo lectura para luego leer su ultima posicion
+            $filaUtilizable = NULL;
+            if (count($arrayDisponibles) > 0) {
+                foreach ($arrayDisponibles as $filaDisponible) {
+                    if (intval($filaDisponible[2]) > $tamContenido) {//se puede utilizar
+                        $filaUtilizable = $filaDisponible;
+                        break;
+                    }
+                }unset($filaDisponible);
+            }
+            if (!is_null($filaUtilizable)) {
+                fseek($detalleArchivo, intval($filaUtilizable[1])); //puntero al inicio de ese dato
+                $inicioContenido = ftell($detalleArchivo);
+                $lineaIndex = "{$filaUtilizable[0]},{$inicioContenido},{$tamContenido},1;";
+                $datoLinea = "{$filaUtilizable[0]},{$nombre},{$autor},{$fecha},,{$clasificacion},{$descripcion},";
+            } else {
+                fseek($detalleArchivo, 0, SEEK_END); //puntero al final del archivo
+                $inicioContenido = ftell($detalleArchivo);
+                $lineaIndex = "{$ultimaLinea},{$inicioContenido},{$tamContenido},1;";
+            }
+            fwrite($detalleIndex, $lineaIndex); //se almacena: id, inicio, tam del contenido, 1 o 0 si esta activo
+            fwrite($detalleArchivo, $datoLinea);
+            fclose($detalleArchivo);
+            $success = 'Datos guardados correctamente.';
+        }
+    }
+
+    fclose($detalleIndex);
+}
+
+
+//************************************EDITANDO ENTRADA****************************************
+else if (isset($_POST['editar'])) {
+
+    $divStyle = "display:block;";
+    $divStyle2 = "display:none;";
+    $divStyleGuardar = "display:none;";
+
+
     $actual = (int) $_POST['id'];
     $detalleIndexUpdate = fopen($indicePath, "r+");
     $detalleArchivoUpdate = fopen($contenidoPath, "r+");
 
-      
+
     while (!feof($detalleIndexUpdate)) {
 
         $line = fgets($detalleIndexUpdate);
@@ -41,15 +150,13 @@ if (isset($_POST['editar'])) {
             }
         }
     }
-     
+} else {
+    $divStyle = "display:none;";
+    $divStyle2 = "display:block;";
+    $divStyleGuardar = "display:none;";
 
 }
 
- else {
-     $divStyle="display:none;";
-     $divStyle2="display:block;";
- }
-     
 
 //***********************************ELIMINANDO ENTRADA***************************************
 if (isset($_POST['eliminar'])) {
@@ -121,12 +228,11 @@ include('comun/header.php');
     <div class="form-style-6" align="center">
         <h1>Datos de mp3</h1>
         <font color="<?php echo isset($alerta) ? 'red' : 'green'; ?>"><?php echo isset($alerta) ? $alerta : $success; ?></font>
-         <?php
-                while (count($arrayActual) > 0) {
-                    $filaActual = array_pop($arrayActual);       
-                }
-                
-         ?>
+<?php
+while (count($arrayActual) > 0) {
+    $filaActual = array_pop($arrayActual);
+}
+?>
         <form action='' method='post' enctype='multipart/form-data'>
             <input type="text" id="nombre" value="<?php echo $filaActual[1]; ?>" name="nombre" placeholder="Nombre" required>
             <!--<input type="file" id="archivo" name="archivo"  accept=".mp3" required>-->
@@ -138,9 +244,26 @@ include('comun/header.php');
         </form>
     </div>
 </div>
-
+<div class="container" style="<?php echo $divStyleGuardar ?>">
+    <a href="home.php">Volver al listado</a>
+    <div class="form-style-6" align="center">
+        <h1>Datos de mp3</h1>
+        <font color="<?php echo isset($alerta) ? 'red' : 'green'; ?>"><?php echo isset($alerta) ? $alerta : $success; ?></font>
+        <form action='' method='post' enctype='multipart/form-data'>
+            <input type="text" id="nombre" name="nombre" placeholder="Nombre" required>
+            <!--<input type="file" id="archivo" name="archivo"  accept=".mp3" required>-->
+            <input type="text" id="autor" name="autor" placeholder="Autor" required>
+            <input type="date" id="fecha" name="fecha" placeholder="Fecha" required value="12/12/2017">
+            <input type="text" id="clasificacion" name="clasificacion" placeholder='Clasificación' required>
+            <textarea name="descripcion" placeholder="Descripción" required></textarea>
+            <input type="submit" name="guardar" value="Guardar">
+        </form>
+    </div>
+</div>
 <div class="container-list" style="<?php echo $divStyle2 ?>">
-    <a href="nueva_entrada.php">Agregar Nuevo</a>
+    <form method="POST">
+        <input type="submit" name="nuevo" value="Agregar Nuevo">
+    </form>
     <div class="form-style-8" align="center">
         <h1>Mis archivos mp3</h1>
         <table width='100%'>
@@ -156,36 +279,37 @@ include('comun/header.php');
                 </tr>
             </thead>
             <tbody>
-                <?php
-                while (count($arrayContenido) > 0) {
+<?php
+while (count($arrayContenido) > 0) {
 
-                    $btnEliminar = "<input type='submit' name='eliminar' value='Eliminar'>";
-                    $btnEditar = "<input type='submit' name='editar' value='Editar'>";
-                    $btnMp3 = '<input type="file" name="archivo"  accept=".mp3" style="width:134px;">';
-                    $btnMp3 .= '<input type="submit" name="subir" value="Subir mp3" >';
-                    $row = array_pop($arrayContenido);
-                    echo '<tr>';
+    $btnEliminar = "<input type='submit' name='eliminar' value='Eliminar'>";
+    $btnEditar = "<input type='submit' name='editar' value='Editar'>";
+    $btnMp3 = '<input type="file" name="archivo"  accept=".mp3" style="width:134px;">';
+    $btnMp3 .= '<input type="submit" name="subir" value="Subir mp3" >';
+    $row = array_pop($arrayContenido);
+    echo '<tr>';
 
-                    echo "<td>{$row[1]}</td>";
-                    echo "<td>{$row[2]}</td>";
-                    echo "<td>{$row[3]}</td>";
-                    echo "<td>{$row[4]}</td>";
-                    echo "<td>{$row[5]}</td>";
-                    echo "<td>{$row[6]}</td>";
-                    echo '<td>';
-                    echo "<form action='' method='post' enctype='multipart/form-data'>";
-                    echo strlen($row[7]) <= 2 ? $btnMp3 : "<span>{$row[7]}</span>";
-                    echo $btnEditar;
-                    echo $btnEliminar;
-                    echo '<input type="hidden" name="id" value="' . $row[0] . '" >';
-                    echo "</form>";
-                    echo '</td>';
-                    echo '</tr>';
-                }
-                ?>
+    echo "<td>{$row[1]}</td>";
+    echo "<td>{$row[2]}</td>";
+    echo "<td>{$row[3]}</td>";
+    echo "<td>{$row[4]}</td>";
+    echo "<td>{$row[5]}</td>";
+    echo "<td>{$row[6]}</td>";
+    echo '<td>';
+    echo "<form action='' method='post' enctype='multipart/form-data'>";
+    echo strlen($row[7]) <= 2 ? $btnMp3 : "<span>{$row[7]}</span>";
+    echo $btnEditar;
+    echo $btnEliminar;
+    echo '<input type="hidden" name="id" value="' . $row[0] . '" >';
+    echo "</form>";
+    echo '</td>';
+    echo '</tr>';
+}
+?>
             </tbody>
         </table>
     </div>
 </div>
-<?php
-include('comun/footer.php');
+                <?php
+                include('comun/footer.php');
+                
