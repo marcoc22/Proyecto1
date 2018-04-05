9f1745a9228ba2fea7c411db3ff2dbe166e10b74
marco <marco@DESKTOP-O9DPP57>
2018-04-04 23:12:36 -0600

    ayuda agregada

--- ayuda.php
+++ ayuda.php
@@ -2,11 +2,37 @@
 include('comun/header.php');
 ?>
 
-<div class="container-list" style="<?php echo $divStyle2 ?>">
-    <a href="nueva_entrada.php">Agregar Nuevo</a>
+<div class="container-list">
+    <h1>Ayuda</h1>
     <div class="form-style-8" align="center">
-        <h1>Mis archivos mp3</h1>
-        
+        <h2>Registro de usuario</h2>
+        <p>Para registrar un nuevo usuario, es necesario completar los datos para posteriormente iniciar sesión.</p>
+        <img src="img/1.png" alt=""/>
+    </div>
+    <div class="form-style-8" align="center">
+        <h2>Inicio de sesión</h2>
+        <p>Completar con usuario y contraseña</p>
+        <img src="img/2.png" alt=""/>
+    </div>
+    <div class="form-style-8" align="center">
+        <h2>Nuevo dato</h2>
+        <p>Para ingresar un nuevo dato, es necesario dar click al botón "Agregar Nuevo", esto lo llevará a un formulario.</p>
+        <img src="img/3.png" alt=""/>
+    </div>
+    <div class="form-style-8" align="center">
+        <h2>Guardando datos</h2>
+        <p>Completado el formulario, al finalizar, se indica que se ha guardado correctamente.</p>
+        <img src="img/4.png" alt=""/>
+    </div>
+    <div class="form-style-8" align="center">
+        <h2>Editar/Eliminar</h2>
+        <p>Cada entrada se puede eliminar o editar. Si se da click en editar, esto lo llevará a un formulario con los datos.</p>
+        <img src="img/5.png" alt=""/>
+    </div>
+    <div class="form-style-8" align="center">
+        <h2>Actualizando datos</h2>
+        <p>Al completar la actualización de datos, lo llevará al listado inicial.</p>
+        <img src="img/6.png" alt=""/>
     </div>
 </div>
 <?php
